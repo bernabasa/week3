f8521dfc1caa20c1a8850891430e9f515cc92c55
Your Name <bernabas.abdeta@yahoo.com>
2022-03-08 20:04:47 -0500

    adding assets

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+function generatePassword () {
+    return 'securedpassword';
+    return String.fromCharCode(Math.floor(Math.random() *26)+97);
+}
+
+
+console.log()
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    
+  
+    passwordText.value = password;
+
+  
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
