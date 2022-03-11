ee882e06b2450afce30fcfbf50bc669bb131593f
Your Name <bernabas.abdeta@yahoo.com>
2022-03-11 18:31:45 -0500

    initial password setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword () {
-    return 'securedpassword';
-    return String.fromCharCode(Math.floor(Math.random() *26)+97);
+    
+// ask legnth
+var askLength = window.prompt("How long do you want your password? It must be in between 8 and 128!");
+var askUppercase = window.confirm ("Would you like to include uppercase characters?");
+
+askLength = parseInt(askLength)
+if(askLength >= 8 && askLength <=128) {
+    console.log(true);
+} else {
+    window.alert("Your input is invailed! please enter a new password.");
 }
+if (askUppercase)
+
+}
+
+// ask uppercase
+// ask lowercase
+// ask for numbers
+// ask for especial characters
+
+
 
 
 console.log()
