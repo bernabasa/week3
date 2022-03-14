3d4f431056e622b9707d75bb14c6e6c55b376c54
Your Name <bernabas.abdeta@yahoo.com>
2022-03-13 22:10:38 -0400

    save project

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,23 @@
 var generateBtn = document.querySelector("#generate");
 function generatePassword () {
     
-// ask legnth
+// asl length
 var askLength = window.prompt("How long do you want your password? It must be in between 8 and 128!");
-var askUppercase = window.confirm ("Would you like to include uppercase characters?");
+
 
 askLength = parseInt(askLength)
+
+
 if(askLength >= 8 && askLength <=128) {
     console.log(true);
 } else {
     window.alert("Your input is invailed! please enter a new password.");
 }
-if (askUppercase)
-
-}
-
-// ask uppercase
-// ask lowercase
-// ask for numbers
-// ask for especial characters
+};
 
 
 
 
-console.log()
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
@@ -34,8 +28,8 @@ function writePassword() {
     passwordText.value = password;
 
   
-  }
+  };
   
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
-  
+
